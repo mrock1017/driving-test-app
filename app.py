60e499e941ced9c04f57414d97aa8286dca2995e
Your Name <your@email.com>
2026-05-20 16:23:28 +0900

    Privacy terms

--- app.py
+++ app.py
@@ -25,8 +25,8 @@ app.config['SECRET_KEY'] = os.environ.get(
 # ✅ DATABASE
 # =========================================================
 
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    'sqlite:///users.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    'DATABASE_URL'
 )
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
