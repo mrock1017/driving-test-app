8a5ca0a4a48db330445cc0c32ec29c6a1a3a9fdb
Your Name <your@email.com>
2026-05-23 16:33:18 +0900

    Payment cancellation update

--- app.py
+++ app.py
@@ -648,6 +648,12 @@ def favicon():
 
 if __name__ == '__main__':
 
+    with app.app_context():
+
+        db.drop_all()
+
+        db.create_all()
+
     port = int(
         os.environ.get("PORT", 5000)
     )
