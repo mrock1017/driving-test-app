b11fc54ec8fce86989de1207b0e5dc3b4450cea3
Your Name <your@email.com>
2026-05-24 20:52:38 +0900

    Update payment stripe

--- app.py
+++ app.py
@@ -108,6 +108,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+with app.app_context():
+
+    db.drop_all()
+
+    db.create_all()
 
 # =========================================================
 # 📧 EMAIL CONFIG
