a03a4c6dafbdd9bbefcf67f82572d8741109ebd0
Your Name <your@email.com>
2026-05-24 20:55:00 +0900

    Payment cancellation update

--- app.py
+++ app.py
@@ -108,11 +108,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
-with app.app_context():
-
-    db.drop_all()
-
-    db.create_all()
 
 # =========================================================
 # 📧 EMAIL CONFIG
